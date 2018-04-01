f7104222eff63921e954799b86d92c0f981f4a95
Nathan Mokhtarzadeh <nathanmo@bu.edu>
2018-04-01 18:26:07 -0400

    finally got the table working

--- teamQuestion.py
+++ teamQuestion.py
@@ -2,19 +2,25 @@ def getArray(seq):
     ret = [[False for x in range(sum(seq) + 1)] for y in range(len(seq))] 
     return ret
 
-def q1(A):
+
+def q1AltAgain(A):
     chart = getArray(A)
-    for r in range(len(chart)):
+    chart[0][0] = True
+    chart[0][A[0]] = True
+    total = sum(A)
+    for r in range(1, len(chart)):
         for c in range(len(chart[0])):
-            if chart[r][c]:
-                continue
-            elif c == 0:
+            if chart[r - 1][c]:
                 chart[r][c] = True
-            elif c == A[r]:
-                chart[r][c] = True
-            elif c > A[r]:
-                continue
-            elif r != 0 and chart[r-1][c]:
-                chart[r][c] = True
-                
+                if A[r] + c <= total:
+                    chart[r][A[r] + c] = True
     return chart
+            
+            
+            
+def printArray(A):
+    for each in A:
+        print(each)
+        
+
+
